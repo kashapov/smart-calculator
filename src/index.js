bc4908e311d3cbbd21d5a66264a8fb347cdfdb5f
Andrey Kashapov <kashapovandrey@gmail.com>
2018-03-13 21:29:44 +0300

    calculator v.1.0

--- src/index.js
+++ src/index.js
@@ -1,27 +1,37 @@
 class SmartCalculator {
+  
   constructor(initialValue) {
-    // your implementation
+    this.str = String(initialValue);
+    this.toString = function() { 
+      return eval(this.str) 
+    };
   }
 
   add(number) {
-    // your implementation
+    this.str += '+' + number;
+		return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.str += '-' + number;
+		return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.str += '*' + number;
+		return this;
   }
 
   devide(number) {
-    // your implementation
+    this.str += '/' + number;
+		return this;
   }
 
   pow(number) {
-    // your implementation
+    this.str += '**' + number;
+		return this;
   }
+
 }
 
 module.exports = SmartCalculator;
